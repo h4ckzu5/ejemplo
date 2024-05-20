dd1cf66b3280a277ecc962b43943008ae42f7737
System Administrator <root@MacBook-Pro-de-h4ckzu5.local>
2024-05-19 20:48:52 -0600

    ADB h4ckzu5

---
+++ pages/header.php
@@ -0,0 +1,54 @@
+<?php
+
+
+session_start();
+// error_reporting(0);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" type="image/png" href="asset/img/favicon.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="asset/css/style.css">
+    <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+
+
+    <link rel="stylesheet" href="asset/css/vanillatoasts.css">
+
+
+    <link rel="stylesheet" href="asset/css/all.min.css">
+    <!-- JavaScript -->
+    <script src="asset/js/app/jquery.min.js"></script>
+    <script src="asset/js/app/alertify.min.js"></script>
+    <link rel="stylesheet" href="asset/css/image.zoom.css">
+    <script src="asset/js/app/bootstrap.bundle.min.js"></script>
+    <script src="asset/js/app/vanillatoasts.js"></script>
+
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="asset/css/alertify.min.css" />
+    <!-- Default theme -->
+    <link rel="stylesheet" href="asset/css/alertify-default.min.css" />
+
+    <!-- Bootstrap theme -->
+    <link rel="stylesheet" href="asset/css/alertify.bootstrap.min.css" />
+
+    <title>h4ckzu5 ADB</title>
+</head>
+
+<body>
+    <!-- Demo header-->
+    <section class="py-5 header">
+        <div class="container py-4">
+            <center>
+    <img src="https://i.imgur.com/mPfDVF7.png" width="16%">
+            <header class="text-center mb-5 pb-5 text-white">
+                <b class="display-1" style="color: white;
+font-weight: bold">يسوع ال إبليس</b></center>
+
+                </p>
+            </header>
